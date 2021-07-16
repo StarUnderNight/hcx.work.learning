7cf47cb9064b862fd0db726066778eaf83dfdf5b
hcx_g <alivingwe@163.com>
2021-07-16 11:42:55 +0800

    Knowledge

---
+++ src/main/java/hcx/work/learning/essential/Knowledge.java
@@ -0,0 +1,20 @@
+package hcx.work.learning.essential;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Knowledge {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void learn() {
+
+	// ArrayList & LinkedList
+	ArrayList<Integer> al;
+	LinkedList<Integer> ll;
+	ArrayList<Integer[]> lis = new ArrayList<Integer[]>();
+    }
+
+}
--- src/main/java/hcx/work/learning/leetcode/Test.java
+++ src/main/java/hcx/work/learning/leetcode/Test.java
@@ -2,17 +2,26 @@ package hcx.work.learning.leetcode;
 
 public class Test<T, K, L> {
     public static void main(String[] args) {
-	int a = 10;
-	int b = a++;
-	int c = ++a;
-	String v = "k";
-    }
 
-    public <T> void get(T a) {
+	new Test().test(13);
+	StringBuilder sb = new StringBuilder("12345");
+	sb.delete(1, 3);
+	System.out.println(sb.toString());
 
     }
-}
 
-interface A<T> {
+    private int age = 10;
 
+    public void test(final int h) {
+	class InClass {
+	    public void inPrint() {
+		System.out.println("h = " + h + "; age = " + age);
+	    }
+	}
+	new InClass().inPrint();
+    }
+
+    private void test(int a, int b) {
+
+    }
 }
