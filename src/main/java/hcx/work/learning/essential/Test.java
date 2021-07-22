647dd5c6b2830c146a93f6cf5e8850503e63d76f
hcx_g <alivingwe@163.com>
2021-07-22 22:00:43 +0800

    Test

---
+++ src/main/java/hcx/work/learning/essential/Test.java
@@ -0,0 +1,20 @@
+package hcx.work.learning.essential;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Test {
+    public static void main(String[] args) {
+	LinkedList<Integer> link = new LinkedList<Integer>();
+	for (int i = 0; i < 10; i++) {
+	    link.add(i);
+	}
+	ArrayList<Integer> list = new ArrayList<Integer>(link);
+
+	Iterator<Integer> iterator = list.iterator();
+	while (iterator.hasNext()) {
+	    System.out.println(iterator.next());
+	}
+    }
+}
